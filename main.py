806e869af82b46546955aa84aff2cda71f8385f9
Joseph Han <top2topii@gmail.com>
2020-02-26 11:32:28 +0900

    오타 수정, 중복 코드제거

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 
-symbols = ["♣","♦","♥","♠"]
-numbers = ["3","4","5","6","7","8","9","10", "J", "Q", "K", "A", "2"]
+symbols = ['♣', '♦', '♥', '♠']
+numbers = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2']
+
 
 class Card:
     def __init__(self, _symbol, _number):
@@ -9,20 +10,20 @@ class Card:
         self.number = _number
 
     def Get(self):
-        return self.symbol + ":" + self.number
+        return self.symbol + ':' + self.number
 
     def __str__(self):
-        return self.symbol + ":" + self.number
+        return self.symbol + ':' + self.number
 
     def __gt__(self, other):
         selfNumberIndex = numbers.index(self.number)
         otherNumberIndex = numbers.index(other.number)
 
         if selfNumberIndex > otherNumberIndex:
-            return  True
+            return True
         elif selfNumberIndex < otherNumberIndex:
             return False
-        else :
+        else:
             selfSymbolIndex = symbols.index(self.symbol)
             otherSymbolIndex = symbols.index(other.symbol)
             if selfSymbolIndex > otherSymbolIndex:
@@ -32,13 +33,11 @@ class Card:
             else:
                 raise ValueError
 
+
 class Deck:
     def __init__(self):
-        print("Deck init")
+        print('Deck init')
         self.deck = []
-        for i in symbols:
-            for j in numbers:
-                self.deck.append(Card(i, j))
 
     def Get(self):
         return self.deck
@@ -50,65 +49,66 @@ class Deck:
     def Shuffle(self):
         random.shuffle(self.deck)
 
+    def FullFill(self):
+        for i in symbols:
+            for j in numbers:
+                self.deck.append(Card(i, j))
+
+    def AddCard(self, aCard):
+        self.deck.append(aCard)
+
+
 class Manager:
     def __init__(self, _playerNumber):
         self.playerNumber = _playerNumber
         self.myDeck = Deck()
+        self.myDeck.FullFill()
         self.myDeck.Shuffle()
 
-    def Greater(self, a, b):
+    @staticmethod
+    def Greater(a, b):
         if a > b:
             return True
-        else :
+        else:
             return False
 
-def TestGt():
-    # 문양 같음, 숫자 다름
-    aCrad = Card(symbols[0],numbers[0])
-    print("aCrad" + str(aCrad))
-    bCrad = Card(symbols[0], numbers[1])
-    print("bCrad" + str(bCrad))
 
-    if aCrad > bCrad:
-        print("aCrad > bCrad")
-    else:
-        print("aCrad <= bCrad")
+def TestGt():
+
+    def PrintCards(a, b):
+        print('---------------------------------')
+        print('aCard:' + str(a) + '\t' + 'bCard:' + str(b))
+
+        if a > b:
+            print('aCard > bCard')
+        else:
+            print('aCard <= bCard')
+
+    # 문양 같음, 숫자 다름
+    aCard = Card(symbols[0], numbers[9])
+    bCard = Card(symbols[0], numbers[3])
+    PrintCards(aCard, bCard)
 
     # 문양 다름, 숫자 다름
-    aCrad = Card(symbols[1], numbers[0])
-    print("aCrad" + str(aCrad))
-    bCrad = Card(symbols[0], numbers[1])
-    print("bCrad" + str(bCrad))
+    aCard = Card(symbols[1], numbers[6])
+    bCard = Card(symbols[2], numbers[4])
+    PrintCards(aCard, bCard)
 
-    if aCrad > bCrad:
-        print("aCrad > bCrad")
-    else:
-        print("aCrad <= bCrad")
-
-    # 문양 같음, 숫자 다름
-    aCrad = Card(symbols[1], numbers[0])
-    print("aCrad" + str(aCrad))
-    bCrad = Card(symbols[1], numbers[1])
-    print("bCrad" + str(bCrad))
-
-    if aCrad > bCrad:
-        print("aCrad > bCrad")
-    else:
-        print("aCrad <= bCrad")
+    # 문양 다름, 숫자 같음
+    aCard = Card(symbols[1], numbers[10])
+    bCard = Card(symbols[2], numbers[10])
+    PrintCards(aCard, bCard)
 
     # 문양 같음, 숫자 같음
-    aCrad = Card(symbols[1], numbers[1])
-    print("aCrad" + str(aCrad))
-    bCrad = Card(symbols[1], numbers[1])
-    print("bCrad" + str(bCrad))
+    aCard = Card(symbols[3], numbers[4])
+    bCard = Card(symbols[3], numbers[4])
+    PrintCards(aCard, bCard)
 
-    if aCrad > bCrad:
-        print("aCrad > bCrad")
-    else:
-        print("aCrad <= bCrad")
 
 def Main():
     manager = Manager(4)
+    TestGt()
+
 
 if __name__ == '__main__':
     Main()
